52840fd0f67506cdde95cf5dde47707f1dd94fa3
lnxpy <lnxpylnxpy@gmail.com>
2022-05-12 01:51:51 +0430

    some patterns defined

---
+++ settings/patterns.py
@@ -0,0 +1,11 @@
+'''
+this file contains all texts' patterns that
+users see on the interface.
+'''
+
+
+SENDING_MESSAGE = '[>>] SENDING MESSAGE: {}'
+
+RECEIVED_MESSAGE = '[<<] RECEIVED MESSAGE: {}'
+
+ERROR_MESSAGE = '[##] ERROR: {}'
